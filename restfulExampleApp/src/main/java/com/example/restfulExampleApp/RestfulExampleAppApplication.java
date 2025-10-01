416d9d9fc22e5dc5a7858a09487af768f9280d34
josrojrom1 <josrojrom@alum.us.es>
2025-10-01 19:52:12 +0200

    Proyecto en Spring Boot web service + restful + actuator

---
+++ restfulExampleApp/src/main/java/com/example/restfulExampleApp/RestfulExampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulExampleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulExampleAppApplication.class, args);
+	}
+
+}
